c14cd34e766331f773df6b96b64d046ddef9b057
rufer7 <marc.rufer@d-fens.net>
2015-12-18 09:30:03 +0100

    assemblyInfo files prepared for next release

--- src/biz.dfch.CS.Appclusive.Api/Properties/AssemblyInfo.cs
+++ src/biz.dfch.CS.Appclusive.Api/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("biz.dfch.CS.Appclusive.Api")]
-[assembly: AssemblyDescription("Service reference to the Appclusive Framework")]
+[assembly: AssemblyDescription("Service Reference to the Appclusive Core Framework")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("d-fens GmbH")]
 [assembly: AssemblyProduct("biz.dfch.CS.Appclusive.Api")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.*")]
+[assembly: AssemblyVersion("1.1.1.*")]
 // [assembly: AssemblyFileVersion("0.0.3.0")]
