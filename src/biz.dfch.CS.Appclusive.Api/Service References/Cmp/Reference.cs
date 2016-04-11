eb9486649b024e043f2a83a7a16e9f0b25c14a08
rufer7 <marc.rufer@d-fens.net>
2016-04-11 10:33:20 +0200

    service reference updated according Appclusive 2.2.1

--- src/biz.dfch.CS.Appclusive.Api/Service References/Cmp/Reference.cs
+++ src/biz.dfch.CS.Appclusive.Api/Service References/Cmp/Reference.cs
@@ -9,7 +9,7 @@
 //------------------------------------------------------------------------------
 
 // Original file name:
-// Generation date: 21.03.2016 15:10:24
+// Generation date: 11.04.2016 10:32:23
 namespace biz.dfch.CS.Appclusive.Api.Cmp
 {
     
@@ -2082,7 +2082,7 @@ namespace biz.dfch.CS.Appclusive.Api.Cmp
     }
 }
 // Original file name:
-// Generation date: 21.03.2016 15:10:24
+// Generation date: 11.04.2016 10:32:24
 namespace biz.dfch.CS.Appclusive.Core.OdataServices.Cmp
 {
     
--- src/biz.dfch.CS.Appclusive.Api/Service References/Diagnostics/Reference.cs
+++ src/biz.dfch.CS.Appclusive.Api/Service References/Diagnostics/Reference.cs
@@ -9,7 +9,7 @@
 //------------------------------------------------------------------------------
 
 // Original file name:
-// Generation date: 21.03.2016 15:10:19
+// Generation date: 11.04.2016 10:32:29
 namespace biz.dfch.CS.Appclusive.Api.Diagnostics
 {
     
@@ -2373,7 +2373,7 @@ namespace biz.dfch.CS.Appclusive.Api.Diagnostics
     }
 }
 // Original file name:
-// Generation date: 21.03.2016 15:10:19
+// Generation date: 11.04.2016 10:32:29
 namespace biz.dfch.CS.Appclusive.Core.OdataServices.Diagnostics
 {
     
