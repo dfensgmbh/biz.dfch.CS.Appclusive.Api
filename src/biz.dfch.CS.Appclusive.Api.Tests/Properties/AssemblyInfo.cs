98c726507adf2678a5a904f081cc32d18c8443db
rufer7 <marc.rufer@d-fens.net>
2016-07-25 15:18:35 +0200

    Assembly version extracted to AssemblyVersion.cs

--- src/biz.dfch.CS.Appclusive.Api.Tests/Properties/AssemblyInfo.cs
+++ src/biz.dfch.CS.Appclusive.Api.Tests/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2fc2b6f7-7d47-4524-897d-d0df796d7c9c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.3.0.*")]
-// [assembly: AssemblyFileVersion("0.0.3.0")]
